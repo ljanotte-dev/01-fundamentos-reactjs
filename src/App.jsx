4c27fd067e1ba39cc92894091fd2345a31299417
Ljanotte <leandro.dev@gmail.com>
2023-01-03 22:45:01 -0300

    fix: Corrigindo components App

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,67 @@
 import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
 import { Post } from "./components/Post";
-
+import styles from "./App.module.css";
 import "./global.css";
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/diego3g.png",
+      name: "Diego Fernandes",
+      role: "CTO @Rocketseat",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galera 👋" },
+      {
+        type: "paragraph",
+        content: "Acabei de subir mais um projeto no meu portifa",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2022-05-03 20:00:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/maykbrito.png",
+      name: "Mayk Brito",
+      role: "Educator @Rocketseat",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galera 👋" },
+      {
+        type: "paragraph",
+        content: "Acabei de subir mais um projeto no meu portifa",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2022-05-10 20:00:00"),
+  },
+];
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post
-        author="Diego Fernades"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quas mollitia alias impedit eligendi nihil quis, natus, quam reiciendis ex, consequatur perspiciatis totam. Dolor molestiae reiciendis labore eius ipsum quam!"
-      />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+          {posts.map((post) => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
+        </main>
+      </div>
     </div>
   );
 }
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,8 +1,101 @@
 export function Post(props) {
+  const [comments, setComments] = useState(["Post muito bacana, hein?!"]);
+
+  const publishedDateFormatted = format(
+    publishedAt,
+    "d 'de' LLLL 'às' HH:mm'h'",
+    { locale: ptBR }
+  );
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true,
+  });
+
+  const isNewCommentEmpty = newCommentText.length === 0;
+
+  function handleCreateNewComment() {
+    event.preventDefault();
+
+    setComments([...comments, newCommentText]);
+    event.target.comment.value = "";
+  }
+
+  function handleNewCommentChange() {
+    event.target.setCustomValidity("");
+    setNewCommentText(event.target.value);
+  }
+
+  function deleteComment(commentToDelete) {
+    const commentWithoutDeleteOne = comment.filter((comment) => {
+      return comment != commentToDelete;
+    });
+    setComments(commentWithoutDeleteOne);
+  }
+
+  function handleNewCommentInvalid() {
+    event.target.setCustomValidity("Esta área é obrigatória!");
+  }
+
   return (
-    <div>
-      <strong>{props.author}</strong>
-      <p>{props.content}</p>
-    </div>
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <Avatar src={author.avatarUrl} />
+          <div className={styles.authorInfo}>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
+          </div>
+        </div>
+        <time
+          title={publishedDateFormatted}
+          dataTime={publishedAt.toISOString()}
+        >
+          {publishedDateRelativeToNow}
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        {content.map((line) => {
+          if (line.type === "paragraph") {
+            return <p key={line.comment}>{line.content}</p>;
+          } else if (line.type === "link") {
+            return (
+              <p key={line.comment}>
+                <a href="#">{line.content}</a>
+              </p>
+            );
+          }
+        })}
+      </div>
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea
+          name="comment"
+          placeholder="Deixe um comentário"
+          value={newCommentText}
+          onChange={handleNewCommentChange}
+          onInvalid={handleNewCommentInvalid}
+          required
+        />
+        <footer>
+          <button type="submit" disabled={isNewCommentEmpty}>
+            Publicar
+          </button>
+        </footer>
+      </form>
+      <div className={styles.commentList}>
+        {comments.map((comment) => {
+          return (
+            <Comment
+              key={comment}
+              comment={comment}
+              deleteComment={onDeleteComment}
+            />
+          );
+        })}
+      </div>
+    </article>
   );
 }
